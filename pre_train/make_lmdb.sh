fca794c5304569ad4caf7191bc4909c0f5c65b14
bona <sbai1@stevens.edu>
2016-07-26 17:34:05 -0400

    adding trains shell

---
+++ pre_train/make_lmdb.sh
@@ -0,0 +1,38 @@
+EXAMPLE=examples/explo_imagenet
+DATA=data/explo/Dungeon_5663
+TOOLS=build/tools
+
+TRAIN_DATA_ROOT=~/caffe/data/explo/Dungeon_5663/train_img/
+VAL_DATA_ROOT=~/caffe/data/explo/Dungeon_5663/test_img/
+
+RESIZE=true
+if $RESIZE; then
+  RESIZE_HEIGHT=256
+  RESIZE_WIDTH=256
+else
+  RESIZE_HEIGHT=0
+  RESIZE_WIDTH=0
+fi
+
+echo "Creating train lmdb..."
+
+GLOG_logtostderr=1 $TOOLS/convert_imageset \
+    --resize_height=$RESIZE_HEIGHT \
+    --resize_width=$RESIZE_WIDTH \
+    --shuffle \
+    $TRAIN_DATA_ROOT/ \
+    $DATA/dungeon_train.txt \
+    $EXAMPLE/explo_train_lmdb
+
+
+echo "Creating val lmdb..."
+
+GLOG_logtostderr=1 $TOOLS/convert_imageset \
+    --resize_height=$RESIZE_HEIGHT \
+    --resize_width=$RESIZE_WIDTH \
+    --shuffle \
+    $VAL_DATA_ROOT \
+    $DATA/dungeon_test.txt \
+    $EXAMPLE/explo_val_lmdb
+
+echo "Done."
